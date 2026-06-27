b0429ce6225c8bda900508a9a0a1603f3e60a657
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-06-27 14:54:04 +0100

    fix(crash): initialize GeckoRuntime and JITController in AppDelegate

--- browser/Reynard/AppDelegate.swift
+++ browser/Reynard/AppDelegate.swift
@@ -1,23 +1,24 @@
 //
-  //  AppDelegate.swift
-  //  Reynard
-  //
-  //  Created by Minh Ton on 1/2/26.
-  //
+//  AppDelegate.swift
+//  Reynard
+//
+//  Created by Minh Ton on 1/2/26.
+//
 
-  import UIKit
+import UIKit
 
-  @UIApplicationMain
-  class AppDelegate: UIResponder, UIApplicationDelegate {
-      
-      func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-          return true
-      }
-      
-      func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-          return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-      }
-      
-      func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
-  }
-  
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GeckoRuntime.main(argc: CommandLine.argc, argv: CommandLine.unsafeArgv)
+        JITController.shared.start()
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+}
