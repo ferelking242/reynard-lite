075580375824f6765a31552429507a345ee49e59
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-06-27 02:42:54 +0100

    fix: add @UIApplicationMain + cache libidevice_ffi to skip rebuild

--- browser/Reynard/AppDelegate.swift
+++ browser/Reynard/AppDelegate.swift
@@ -1,21 +1,23 @@
 //
-//  AppDelegate.swift
-//  Reynard
-//
-//  Created by Minh Ton on 1/2/26.
-//
+  //  AppDelegate.swift
+  //  Reynard
+  //
+  //  Created by Minh Ton on 1/2/26.
+  //
 
-import UIKit
+  import UIKit
 
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
-    
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-    
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
-}
+  @UIApplicationMain
+  class AppDelegate: UIResponder, UIApplicationDelegate {
+      
+      func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+          return true
+      }
+      
+      func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+          return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+      }
+      
+      func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+  }
+  
