e7da3b533f1ce46888c6940f32bb71ea94956f99
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-06-27 16:52:46 +0100

    fix: remove @UIApplicationMain and GeckoRuntime.main() from AppDelegate

--- browser/Reynard/AppDelegate.swift
+++ browser/Reynard/AppDelegate.swift
@@ -6,13 +6,10 @@
 //
 
 import UIKit
-import GeckoView
 
-@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GeckoRuntime.main(argc: CommandLine.argc, argv: CommandLine.unsafeArgv)
         JITController.shared.start()
         return true
     }
